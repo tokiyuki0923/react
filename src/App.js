ba592b7cebe3f6475ce10dc5d0c7ed929fc79921
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-05-07 22:25:33 +0900

    コメントアウトの方法も変わった。jsファイル使ってるのに。propsというものを使用してみる

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import Todolist from "./Todolist";
 
 function App() {
+  const [todos,setTodos] = useState(["Todo1","Todo2"]);
   return (
     <div>
-      <Todolist />
+      {//{}内のtodosがconstで定義している変数
+      //それをTodolist.jsファイルにtodosという名前で渡しますよという処理
+      }
+      <Todolist todos={todos}/> 
       <input type="text" />
       <button>追加</button>
       <button>完了済みの削除</button>
-      <div></div>
+      <div>残りのタスク:0</div>
     </div>
   );
 }
