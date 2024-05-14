c1a8944c1764ca3cb07528ca55d9121a4d4b5f97
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-05-14 22:11:41 +0900

    あとエイリアスでインポートすることができるようになる

--- src/App.js
+++ src/App.js
@@ -48,3 +48,4 @@ const handleClear = () =>{
 
 export default App;
 //export defaultとexportの違いはコンポーネントを一つだけエクスポートするのか、複数エクスポートするのかという違い。書き方も違いがあり、defaultの場合は一つしかエクスポートしないので{}で囲む必要がない
+//あと、エイリアスでインポートすることができる
