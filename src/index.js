d3d06251c4566b013e8e5fef9973430ba44ea7ee
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-05-14 20:49:36 +0900

    Reactの環境構築した際のindex.jsに最初にimportされているものがなんなのか調べてみたが、結局よくわからなかった。推測を一応コメントアウトしておいたが多分違う。そもそも最初に何をインポートされているのかまでは現時点では把握しなくていいものなのだろうか

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';//react-domパッケージのclient.jsファイルを読み込んでいるのかなと思ったが違った。
 import './index.css';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {
+      //ここでApp.jsの関数Appをstrictmodeで起動している
+    }
   </React.StrictMode>
 );
 
